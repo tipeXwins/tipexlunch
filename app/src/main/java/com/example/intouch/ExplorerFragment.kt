43e1e7c1190c42f143dd49347cda6e526a5334ed
tipeXwins <victorymuchi2013@gmail.com>
2021-05-15 20:16:16 +0200

    s

--- app/src/main/java/com/example/intouch/ExplorerFragment.kt
+++ app/src/main/java/com/example/intouch/ExplorerFragment.kt
@@ -20,9 +20,9 @@ class ExplorerFragment(): Fragment(){
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-       // view.findViewById<Button>(R.id.buttontron).setOnClickListener {
-         //   findNavController().navigate(R.id.ruta)
-       // }
+       view.findViewById<Button>(R.id.buttontron).setOnClickListener {
+            findNavController().navigate(R.id.ruta)
+        }
     }
 
  }
