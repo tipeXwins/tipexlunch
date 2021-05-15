66b941d08e6c4f3a5c30ad03490a3c6c0d9fb4e8
Edison Jair Bejarano Sepulveda <ejbejaranos@gmail.com>
2021-05-15 19:22:39 +0200

    Meeting date uno

--- app/src/main/java/com/example/intouch/MainActivity.kt
+++ app/src/main/java/com/example/intouch/MainActivity.kt
@@ -11,7 +11,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.fragment_join)
+        setContentView(R.layout.fragment_editor)
 
   /*      findViewById<FloatingActionButton>(R.id.fab).setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
